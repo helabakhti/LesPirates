c42f76ddf96693c0ac23032a1200d2d0d451cf9e
helabakhti <bbkhela@gmail.com>
2025-02-19 21:25:35 +0100

    affichage1

--- src/vue/Afficheur.java
+++ src/vue/Afficheur.java
@@ -20,20 +20,20 @@ public class Afficheur {
 
     public void afficherCarte(String[] carte) {
         for (int i = 0; i < carte.length; i++) {
-            System.out.println("La " + (i + 1) + "ème carte est " + carte[i]);
+            System.out.println("La carte"+" "+ (i + 1) + " : " +  carte[i]);
         }
     }
 
     public int choisirCarte(int maxCartes) {
         int numCarte;
         do {
-            System.out.println("Entrer le numéro de la carte (entre 1 et " + maxCartes + ") : ");
-            while (!scanner.hasNextInt()) {  // Vérifie si l'entrée est un nombre
+            System.out.println("Entrer le numéro de la carte à deposer: ");
+            while (!scanner.hasNextInt()) {  
                 System.out.println("Veuillez entrer un nombre valide.");
-                scanner.next(); // Consomme l'entrée invalide
+                scanner.next(); 
             }
             numCarte = scanner.nextInt();
-        } while (numCarte < 1 || numCarte > maxCartes); // Vérifie la validité du choix
+        } while (numCarte < 1 || numCarte > maxCartes); 
         return numCarte;
     }
 
@@ -41,21 +41,21 @@ public class Afficheur {
         System.out.print("Entrez le nom du joueur : ");
         String nomJoueur = scanner.nextLine();
 
-        // Définition des cartes avec descriptions
-        String[] carte = { "Abordage Réussi +2 Popularité", 
-                           "Coup de sabre -2 Vie", 
-                           "Révolte organisée +1 Popularité" ,
-                           "Main de Fer +2 Popularité, -1 Vie" , 
-                           "Discours Insirant +1 Popularité"};
+        // les cartes avec descriptions
+        String[] carte = { "Abordage Réussi (+2 Popularité)", 
+                           "Coup de sabre (-2 Vie)", 
+                           "Révolte organisée (+1 Popularité)" ,
+                           "Main de Fer (+2 Popularité, -1 Vie)" , 
+                           "Discours Insirant (+1 Popularité)"};
 
         afficherNomJoueur(nomJoueur);
         afficherEtatJoueur(nomJoueur, 5, 0);
         afficherCarte(carte);
 
-        // L'utilisateur choisit une carte
+        // le joueur choisit une carte
         int numCarte = choisirCarte(carte.length);
 
-        // Affiche la description de la carte choisie
-        System.out.println("Vous avez choisi : " + carte[numCarte - 1]);
+        // description de la carte deposé
+        System.out.println("Carte deposée : " + carte[numCarte - 1]);
     }
 }
